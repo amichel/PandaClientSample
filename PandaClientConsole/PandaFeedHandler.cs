89c3cea66ea1bfbcb3718d0b8c4e68149cefd307
Alex Michel <amishel@gmail.com>
2017-08-15 00:35:15 +0300

    Panda client sample with dotnetty

---
+++ PandaClientConsole/PandaFeedHandler.cs
@@ -0,0 +1,20 @@
+﻿using DotNetty.Transport.Channels;
+using System;
+
+namespace PandaClientConsole
+{
+    public class PandaFeedHandler : SimpleChannelInboundHandler<string>
+    {
+        protected override void ChannelRead0(IChannelHandlerContext contex, string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public override void ExceptionCaught(IChannelHandlerContext contex, Exception e)
+        {
+            Console.WriteLine(DateTime.Now.Millisecond);
+            Console.WriteLine("{0}", e.StackTrace);
+            contex.CloseAsync();
+        }
+    }
+}
---
+++ PandaClientConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using DotNetty.Codecs;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+using System.Configuration;
+
+namespace PandaClientConsole
+{
+    class Program
+    {
+        static async Task RunClientAsync()
+        {
+
+            var group = new MultithreadEventLoopGroup();
+            try
+            {
+                var bootstrap = new Bootstrap();
+                bootstrap
+                    .Group(group)
+                    .Channel<TcpSocketChannel>()
+                    .Option(ChannelOption.TcpNodelay, true)
+                    .Handler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+                        pipeline.AddLast(new DelimiterBasedFrameDecoder(8192, Delimiters.LineDelimiter()));
+                        pipeline.AddLast(new StringEncoder(), new StringDecoder(), new PandaFeedHandler());
+                    }));
+
+                IChannel bootstrapChannel = await bootstrap.ConnectAsync(new IPEndPoint(IPAddress.Parse(ConfigurationManager.AppSettings["host"]), int.Parse(ConfigurationManager.AppSettings["port"])));
+                Console.ReadLine();
+              
+                await bootstrapChannel.CloseAsync();
+            }
+            finally
+            {
+                group.ShutdownGracefullyAsync().Wait(1000);
+            }
+        }
+
+        static void Main() => RunClientAsync().Wait();
+    }
+}
